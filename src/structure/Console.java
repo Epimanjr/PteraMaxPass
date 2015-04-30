91bf09488fccda8c2b91b55d6a406f9443992284
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-04-30 10:39:47 +0200

    Add a first console version

---
+++ src/structure/Console.java
@@ -0,0 +1,92 @@
+package structure;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maxime BLAISE
+ */
+public class Console {
+
+    /**
+     * ListPass object used in the program.
+     */
+    public static ListPass lp;
+
+    /**
+     * Scanner in order to read user line.
+     */
+    public static Scanner sc = new Scanner(System.in);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        initListPass();
+        runAutomate();
+    }
+
+    /**
+     * Initialize HashMap used during the program.
+     */
+    private static void initListPass() {
+        System.out.println("Try to init ListPass object from " + ListPass.filename + " ... ");
+        lp = ListPass.readFromFile();
+        if (lp == null) {
+            System.out.print("Initialize new empty map instead ... ");
+            lp = new ListPass();
+            System.out.println("Done.");
+        } else {
+            System.out.println("OK");
+        }
+    }
+
+    /**
+     * Automate.
+     */
+    private static void runAutomate() {
+        // Ask for a line
+        System.out.print("Console:$ ");
+        String line = sc.nextLine();
+        // Loop
+        while (!line.equals("exit")) {
+            switch(line) {
+                case "ls":
+                    lp.lister();
+                    break;
+                case "add":
+                    System.out.print("Key => ");
+                    String key = sc.nextLine();
+                    System.out.print("Login => ");
+                    String login = sc.nextLine();
+                    System.out.print("Password => ");
+                    String password = sc.nextLine();
+                    lp.addAndSave(key, login, password);
+                    break;
+                default:
+                    String[] lines = line.split(" ");
+                    switch(lines[0]) {
+                        case "get":
+                            if(lines.length >= 2) {
+                                System.out.println(lp.getPass(lines[1]));
+                                ModifPressPap mpp = new ModifPressPap();
+                                mpp.setClipboardContents(lp.getPass(lines[1]));
+                            }
+                            break;
+                        case "del":
+                            if(lines.length >= 2) {
+                                lp.delAndSave(lines[1]);
+                            }
+                            break;
+                    }
+                    break;
+            }
+            // Ask for new line
+            System.out.print("Console:$ ");
+            line = sc.nextLine();
+        }
+    }
+
+}
