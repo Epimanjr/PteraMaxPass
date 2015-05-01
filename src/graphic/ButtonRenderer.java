42fa881d5ceffb217f6176fae6f040853ab6b60a
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-05-01 22:30:39 +0200

    Commit interface

---
+++ src/graphic/ButtonRenderer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphic;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author Maxime
+ */
+public class ButtonRenderer extends JButton implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean isFocus, int row, int col) {
+      //On écrit dans le bouton avec la valeur de la cellule
+      setText((value != null) ? value.toString() : "");
+      //On retourne notre bouton
+      return this;
+   }
+    
+}
